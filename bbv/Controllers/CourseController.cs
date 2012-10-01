3f893dbcc61a8a810d0e02e4f636de5d073f529f
Ayende Rahien <ayende@ayende.com>
2012-10-01 11:54:13 +0200

    Adding data to the db

---
+++ bbv/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using bbv.Models;
+
+namespace bbv.Controllers
+{
+	public class CourseController : RavenController
+	{
+		 public object New(string name)
+		 {
+			 var course = new Course
+				 {
+					 Name = name
+				 };
+
+			Session.Store(course);
+
+			 return Json(course.Id);
+		 }
+	}
+}
---
+++ bbv/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace bbv.Models
+{
+	public class Course
+	{
+		public string Id { get; set; }
+		public string Name { get; set; } 
+	}
+}
