b8b1b676735cefe83401681bf4f3786330ae6500
Philipp Dolder <philipp.dolder@gmail.com>
2012-10-01 13:41:50 +0200

    Added Student & User

---
+++ bbv/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace bbv.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Id { get; set; }
+    }
+}
