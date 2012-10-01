c58470e7e666c1994e1b91fdcc80155b7320f467
Ayende Rahien <ayende@ayende.com>
2012-10-01 18:04:03 +0200

    Adding an order

---
+++ bbv/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace bbv.Models
+{
+	public class Order
+	{
+		public string Id { get; set; }
+		public string CourseId { get; set; }
+		public decimal Cost { get; set; }
+		public int Participants { get; set; }
+	}
+}
