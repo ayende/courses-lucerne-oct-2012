0d920fa9520a0982589fab243c167400d4f3fba7
Ayende Rahien <ayende@ayende.com>
2012-10-02 10:30:54 +0200

    Adding animals

---
+++ bbv/Controllers/WoofController.cs
@@ -0,0 +1,32 @@
+﻿using bbv.Models;
+
+namespace bbv.Controllers
+{
+	public class WoofController : RavenController
+	{
+		public object Populate()
+		{
+			Session.Store(new Animal
+				{
+					Name = "Arava",
+					Species = "Dog",
+					Breed = "German Shepherd"
+				});
+			Session.Store(new Animal
+				{
+					Name = "Oscar",
+					Species = "Dog",
+					Breed = "Little Mixed"
+				});
+
+			Session.Store(new Animal
+				{
+					Name = "Meow",
+					Species = "Cat",
+					Breed = "Street"
+				});
+
+			return Json("Okay");
+		}
+	}
+}
---
+++ bbv/Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace bbv.Models
+{
+	public class Animal
+	{
+		public string Name { get; set; }
+		public string Species { get; set; }
+		public string Breed { get; set; }
+	}
+
+}
